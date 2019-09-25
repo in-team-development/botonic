e0c03ca796888928ac91fa7dde04819d75c48c80
Pawel Walczak <pawel.walczak@infront.com.pl>
2019-09-25 12:36:16 +0200

    Refactor serve command to work on windows

--- packages/botonic-cli/src/commands/serve.ts
+++ packages/botonic-cli/src/commands/serve.ts
@@ -1,30 +1,26 @@
-import { Command, flags } from '@oclif/command'
+import { Command } from '@oclif/command'
 
 import { track } from '../utils'
 import * as colors from 'colors'
-
-import { spawn, exec } from 'child_process'
+import { spawn } from 'child_process'
 
 export default class Run extends Command {
-  static description = 'Serve your bot in your localhost'
-
-  static examples = [
-    `$ botonic serve\n> Project is running at http://localhost:8080/`
-  ]
-
-  static flags = {}
-
-  static args = []
-
-  private botonic: any
 
   async run() {
     track('Served Botonic CLI')
-    const { args, flags } = this.parse(Run)
 
     try {
-      exec('npm run start'])
+      const serve = spawn('npm', ['run', 'start'], {shell: true})
       console.log(colors.blue('\nServing Botonic...'))
+      serve.stdout.on('data', out => {
+        console.log(`${out}`)
+      })
+      serve.stderr.on('data', stderr => {
+        console.log(colors.red(`${stderr}`))
+      })
+      serve.on('close', code => {
+        console.log(colors.red(`child process exited with code ${code}`))
+      })
     } catch (e) {
       console.log(e)
     }
